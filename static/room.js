789957922caf417d709b11a48abbf3c3f6e59c4a
Sanjarbek <sanjarbeksaminjonovv@gmail.com>
2022-09-23 23:45:23 +0500

    comment

--- static/room.js
+++ static/room.js
@@ -7,20 +7,20 @@ let chatMessageInput = document.querySelector("#chatMessageInput");
 let chatMessageSend = document.querySelector("#chatMessageSend");
 let onlineUsersSelector = document.querySelector("#onlineUsersSelector");
 
-// adds a new option to 'onlineUsersSelector'
-function onlineUsersSelectorAdd(value) {
-    if (document.querySelector("option[value='" + value + "']")) return;
-    let newOption = document.createElement("option");
-    newOption.value = value;
-    newOption.innerHTML = value;
-    onlineUsersSelector.appendChild(newOption);
-}
+// // adds a new option to 'onlineUsersSelector'
+// function onlineUsersSelectorAdd(value) {
+//     if (document.querySelector("option[value='" + value + "']")) return;
+//     let newOption = document.createElement("option");
+//     newOption.value = value;
+//     newOption.innerHTML = value;
+//     onlineUsersSelector.appendChild(newOption);
+// }
 
-// removes an option from 'onlineUsersSelector'
-function onlineUsersSelectorRemove(value) {
-    let oldOption = document.querySelector("option[value='" + value + "']");
-    if (oldOption !== null) oldOption.remove();
-}
+// // removes an option from 'onlineUsersSelector'
+// function onlineUsersSelectorRemove(value) {
+//     let oldOption = document.querySelector("option[value='" + value + "']");
+//     if (oldOption !== null) oldOption.remove();
+// }
 
 // focus 'chatMessageInput' when user opens the page
 chatMessageInput.focus();
@@ -63,7 +63,7 @@ function connect() {
         console.log(data);
         switch (data.type) {
             case "chat_message":
-                chatLog.value += data.user + ": " + data.message + "\n";  // new
+                chatLog.value += data.user + ": " + data.message + "\n";
                 break;
             default:
                 console.error("Unknown message type!");
