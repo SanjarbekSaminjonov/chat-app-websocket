509a796d60d969f41e05eaedf522db93d0e06f90
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2022-09-29 10:48:49 +0500

    connect psql

--- core/settings.py
+++ core/settings.py
@@ -87,8 +87,12 @@ ASGI_APPLICATION = 'core.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': env.str('DB_HOST'),
+        'PORT': env.str('DB_PORT'),
     }
 }
 
