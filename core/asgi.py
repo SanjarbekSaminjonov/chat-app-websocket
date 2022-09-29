9a1707aebdc617d97c95fbf4f5d5136480a6d371
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2022-09-29 10:18:13 +0500

    Fix asgi.py to run by daphne

--- core/asgi.py
+++ core/asgi.py
@@ -8,17 +8,19 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django_asgi_app = get_asgi_application()
+
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from app.routing import ws_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns)),
-    # 'websocket': URLRouter(ws_urlpatterns),
 })
