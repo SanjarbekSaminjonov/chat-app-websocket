b802ecf10a1368faf11b99109f67668577b32979
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2022-09-29 10:29:01 +0500

    static root settings

--- core/settings.py
+++ core/settings.py
@@ -129,9 +129,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
